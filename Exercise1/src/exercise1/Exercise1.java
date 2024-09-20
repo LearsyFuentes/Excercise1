05b09cbeb3b49038c96cee527be7fd50101e60de
mi.defuentes <mi.defuentes@106-07.info2>
2024-09-20 13:44:28 +0200

    Ejercicio 1 terminado

---
+++ Exercise1/src/exercise1/Exercise1.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package exercise1;
+
+/**
+ * 
+ */
+public class Exercise1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello, how are you? ");
+		System.out.println("Fine thanks.");
+
+	}
+
+}
